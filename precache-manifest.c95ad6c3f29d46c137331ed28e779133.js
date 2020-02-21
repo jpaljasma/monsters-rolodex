f5bd4c8f7af26fb2420d07dbeb5a4b0b1189c6c1
Jaan Paljasma <jaan@hebsdigital.com>
2020-02-21 16:34:59 -0500

    Updates

---
+++ precache-manifest.c95ad6c3f29d46c137331ed28e779133.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b53558269d447a3b9d4f957ffb697945",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "48a28a4a74d055dd9945",
+    "url": "/monsters-rolodex/static/css/main.72827082.chunk.css"
+  },
+  {
+    "revision": "df2d7372a42d17961790",
+    "url": "/monsters-rolodex/static/js/2.b8f60689.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.b8f60689.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48a28a4a74d055dd9945",
+    "url": "/monsters-rolodex/static/js/main.9e0d0d4b.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
